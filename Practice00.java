2312ff1288b80ec99060c0dd8e9d22fa570cfc65
Oleksiy Korniyenko (Germany) <korniyenkooleksiy@gmail.com>
2025-03-20 13:10:59 +0100

    Update Practice00.java
    Add text

--- Practice00.java
+++ Practice00.java
@@ -2,6 +2,9 @@ public class Practice00 {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Hello fromm Oleksiy Korniyenko");
+        
+
     }
 
 }
